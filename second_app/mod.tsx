d38c4042809c5180b00cef3a97a927897fb06a92
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-02 01:52:02 +0900

    modify

--- second_app/mod.tsx
+++ second_app/mod.tsx
@@ -1,7 +1,7 @@
 /* @jsx React.createElement */
 
 // @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
-import { FC } from "https://dev.jspm.io/react";
+import React, { FC } from "https://dev.jspm.io/react";
 // @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from "https://dev.jspm.io/react-dom/server";
 
