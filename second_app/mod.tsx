26667046a6eac457720601f023c3a498aa53cc6c
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-02 01:50:09 +0900

    modify

--- second_app/mod.tsx
+++ second_app/mod.tsx
@@ -1,4 +1,8 @@
+/* @jsx React.createElement */
+
+// @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
 import { FC } from "https://dev.jspm.io/react";
+// @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from "https://dev.jspm.io/react-dom/server";
 
 const View: FC = () => (
